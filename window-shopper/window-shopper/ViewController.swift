facba23369677807c0cccf34ec59ea360569a498
rlopez178 <rlopez178@yahoo.com>
2017-09-07 23:30:45 -0400

    initial commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Raymond Lopez on 9/7/17.
+//  Copyright © 2017 OceanBiscuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
